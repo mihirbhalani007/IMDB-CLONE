fcc2932bc744bc00820e8f0f6b84120ebc13aaf2
Mihir Patel <mihirbhalani18@gmail.com>
2024-06-03 17:01:24 +0530

    First commit & working on routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import MovieList from "./components/MovieList/MovieList";
+import Movie from "./pages/MovieDetail/Movie";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Header>
+            <Routes>
+              <Route index element={<Home />}></Route>
+              <Route path="movie/:id" element={<Movie />}></Route>
+              <Route path="movie/:type" element={<MovieList />}></Route>
+              <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+          </Header>
+        </Router>
+      </div>
+    </>
   );
 }
 
